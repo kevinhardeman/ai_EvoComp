7c1e3ea09048fe1190722667441710551547affa
Simone van Bruggen <simonevanbruggen@hotmail.com>
2018-09-20 13:48:27 +0200

    Add sort method for array of Elephant
    Note: only works on arrays (so not on arraylist etc)

--- Elephant.java
+++ Elephant.java
@@ -1,7 +1,8 @@
 import java.io.File;
+import java.util.Comparator;
 
-// TODO: betere objectnaam? :D
-public class Elephant {
+
+public class Elephant implements Comparable<Object>{
 	public double[] values = new double[10];
 	public double fitness = 0.0;
 	public int index = 0; // check if needed
@@ -9,6 +10,15 @@ public class Elephant {
 	public Elephant(double[] values, int index){
 		this.values = values;
 		this.index = index;
+	}
+
+	public double getFitness(){
+		return this.fitness;
+	}
+
+	public int compareTo(Object e) {
+		Elephant elephant = (Elephant)e;
+		return Double.compare(elephant.getFitness(), this.fitness);
 	}
 
 }
