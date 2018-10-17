91f9e065245c55f91473fbbf7243afe377f9a60f
Bram Kraaijeveld <bram.kraaijeveld@gmail.com>
2018-10-17 10:28:17 +0200

    add completely random baseline

--- player40.java
+++ player40.java
@@ -14,6 +14,10 @@ public class player40 implements ContestSubmission
 
 	static boolean DEBUG = false;
 	static boolean GET_TEST_RESULTS = true;
+
+	// Completely Random Baseline
+	static boolean COMPLETELY_RANDOM = false;
+
 	static int DIMENSION = 11;
 	static double MAX_RANGE = 5.0;
 
@@ -94,68 +98,87 @@ public class player40 implements ContestSubmission
 	}
 
 	public void run() {
-		Elephant[] population = initiate(population_size, max_sigma);
+		if (COMPLETELY_RANDOM) {
 
-		Elephant[] totaList = new Elephant[0]; //totaList
-        Elephant[] noveList = initiate(1, max_sigma); //list to keep track of all previously novel elephants. the first initiate is just te avoid nullpointerexceptions (could be implemented better)
-  		for (int i=0; i<evaluations_limit; i++){
-  			int noveListElephant = 0;
-			Elephant[] children = new Elephant[population_size];
+			double max_score = 0.0;
 
-			for (int j=0; j<population_size; j++) {
-				// Select Parents from population
-				Elephant[] parents = select(population, 2, tournament_size, noveList, linearblend, nearestNeighbours);
+			for (int i=0; i<evaluations_limit; i++) {
+				double[] totally_random_point = new double[DIMENSION-1];
+				for (int j=0; j<DIMENSION-1; j++)
+					totally_random_point[j] = randomDouble(-MAX_RANGE, MAX_RANGE);
+					double score = (double) evaluation.evaluate(totally_random_point);
 
-				// Create child by mating parents and mutating result
-				children[j] = mutate(mate(parents[0], parents[1], crossover_points), mutation_probability);
+					if (score > max_score){
+						max_score = score;
+						System.out.println(max_score);
+					}					
 			}
+		}
+		else {
 
-			totaList = concatenate(population, noveList);
-			for(int e = 0; e < children.length; e++){
-				if(children[e].getNovelty(totaList, nearestNeighbours) > novelty_treshold){
-					append(noveList, children[e]);
-					append(totaList, children[e]);
+			Elephant[] population = initiate(population_size, max_sigma);
+
+			Elephant[] totaList = new Elephant[0]; //totaList
+	        Elephant[] noveList = initiate(1, max_sigma); //list to keep track of all previously novel elephants. the first initiate is just te avoid nullpointerexceptions (could be implemented better)
+	  		for (int i=0; i<evaluations_limit; i++){
+	  			int noveListElephant = 0;
+				Elephant[] children = new Elephant[population_size];
+
+				for (int j=0; j<population_size; j++) {
+					// Select Parents from population
+					Elephant[] parents = select(population, 2, tournament_size, noveList, linearblend, nearestNeighbours);
+
+					// Create child by mating parents and mutating result
+					children[j] = mutate(mate(parents[0], parents[1], crossover_points), mutation_probability);
+				}
+
+				totaList = concatenate(population, noveList);
+				for(int e = 0; e < children.length; e++){
+					if(children[e].getNovelty(totaList, nearestNeighbours) > novelty_treshold){
+						append(noveList, children[e]);
+						append(totaList, children[e]);
+					}
+				}
+
+				population = concatenate(population, children);
+				population = select(population, population_size, tournament_size, noveList, linearblend, nearestNeighbours);
+				Arrays.sort(population);
+
+	            //TODO: Slowly moves the linearblend function to 1.
+	            if(linearblend < 1){
+	                linearblend += linearblend_delta;
+	            }
+
+	            // Debug Printout
+	            if (DEBUG) {
+		            /*
+		            double mean_novelty = 0.0;
+		            for (Elephant e : population) {
+		            	mean_novelty += Math.abs(e.getNovelty(population, nearestNeighbours));
+		            }
+		            mean_novelty /= population.length;
+					*/
+		            double mean_sigma = 0.0;
+		            for (Elephant e : population) {
+		            	mean_sigma += e.getValues()[DIMENSION-1];
+		            }
+		            mean_sigma /= population.length;
+
+		            System.out.print(i);
+		            System.out.print(":\t");
+		            System.out.print(population[0].getFitness());
+		            System.out.print("\t\t");
+		            System.out.print(population[noveListElephant].getNovelty(totaList, nearestNeighbours));
+		            System.out.print("\t\t");
+		            System.out.print(mean_sigma);
+		            System.out.println();
+	            }
+				if (GET_TEST_RESULTS){
+					for (Elephant e : population){
+							System.out.println(e.getFitness());
+					}
 				}
 			}
-
-			population = concatenate(population, children);
-			population = select(population, population_size, tournament_size, noveList, linearblend, nearestNeighbours);
-			Arrays.sort(population);
-
-            //TODO: Slowly moves the linearblend function to 1.
-            if(linearblend < 1){
-                linearblend += linearblend_delta;
-            }
-
-            // Debug Printout
-            if (DEBUG) {
-	            /*
-	            double mean_novelty = 0.0;
-	            for (Elephant e : population) {
-	            	mean_novelty += Math.abs(e.getNovelty(population, nearestNeighbours));
-	            }
-	            mean_novelty /= population.length;
-				*/
-	            double mean_sigma = 0.0;
-	            for (Elephant e : population) {
-	            	mean_sigma += e.getValues()[DIMENSION-1];
-	            }
-	            mean_sigma /= population.length;
-
-	            System.out.print(i);
-	            System.out.print(":\t");
-	            System.out.print(population[0].getFitness());
-	            System.out.print("\t\t");
-	            System.out.print(population[noveListElephant].getNovelty(totaList, nearestNeighbours));
-	            System.out.print("\t\t");
-	            System.out.print(mean_sigma);
-	            System.out.println();
-            }
-						if (GET_TEST_RESULTS){
-							for (Elephant e : population){
-									System.out.println(e.getFitness());
-							}
-						}
 		}
 	}
 
