ef2682245ad9c40937f4708fbbec02f0d2a51749
Simone van Bruggen <simonevanbruggen@hotmail.com>
2018-10-17 11:23:52 +0200

    Messed it up... fixed it

--- player40.java
+++ player40.java
@@ -111,7 +111,7 @@ public class player40 implements ContestSubmission
 					if (score > max_score){
 						max_score = score;
 						System.out.println(max_score);
-					}					
+					}
 			}
 		}
 		else {
@@ -172,11 +172,12 @@ public class player40 implements ContestSubmission
 		            System.out.print("\t\t");
 		            System.out.print(mean_sigma);
 		            System.out.println();
-	            }
+						}
 						if (GET_TEST_RESULTS){
 							// print fitness of best elephant
 							System.out.println(population[population.length-1].getFitness());
 							}
+				}
 		}
 	}
 
